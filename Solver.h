b146a773f9970eea47c0a8f095316f4f79b398ed
inbar8 <inbarmor88@gmail.com>
2019-01-06 20:43:17 +0200

    opened server, MySerialServer, MyParallelServer, Solver

--- Solver.h
+++ Solver.h
@@ -2,4 +2,12 @@
 #ifndef ED2_SOLVER_H
 #define ED2_SOLVER_H
 
+using namespace std;
+
+template<class Problem, class Solution>
+class Solver {
+    virtual Solution solve(Problem problemToSolve) = 0;
+
+};
+
 #endif //ED2_SOLVER_H
